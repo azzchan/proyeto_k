d71ea9de6c66b6bc2fd5e022afcadc43754787af
a_zuzu <elianwebster0@gmail.com>
2023-03-07 23:44:06 -0500

    v1.1.2a

--- main.py
+++ main.py
@@ -1,32 +1,80 @@
 from notifypy import Notify
 from bs4 import BeautifulSoup
 import requests
-
-html_crudo = "https://notify.moe/anime/fxRlqPDVR"
-resultado = requests.get(html_crudo)
-contenido = resultado.text
-soup = BeautifulSoup(contenido, "html.parser")
+import re
+import datetime
+import time
 
 
-arreglo_elementos = soup.find_all("a", {"data-mountable-type": "episode"})
-arreglo_str = [str(i) for i in arreglo_elementos]
+def main():
+    url = "https://notify.moe/anime/fxRlqPDVR"
+    try:
+        latest_episode = get_latest_episode(url)
+        if latest_episode == "true":
+            timer = get_restTime(url)
+            name_anime = get_Name(url)
+            episode_number = latest_episode[1]
+            send_notification("Anime Notification", f"El Episodio {episode_number} de {name_anime} está disponible.")
+        else:
+            timer = get_restTime(url)
+            name_anime = get_Name(url)
+            episode_number = latest_episode[1]
+            send_notification("Anime Notification", f"El Episodio {episode_number} de {name_anime} no está disponible restan {timer} días.")
+    except requests.exceptions.RequestException as exceptMessage:
+        print("Ocurrió un error al hacer la solicitud GET:", exceptMessage)
 
-contador = 0
-string = 'data-available="false"'
-for elemento in arreglo_str:
-    if string in elemento:
-        True
-    else:
-        contador += 1
+def get_restTime(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    timer = soup.find_all("a", {"data-available": "false"})
 
+    for episode in timer:
+        if episode.get("data-available") == "false":
+            anime_timer = episode.text
+            match = re.search(r'.*(\w{3}, \d{2} \w{3} \d{4})', anime_timer)
+            if match:
+                date_str = match.group(1)
+                date = datetime.datetime.strptime(date_str, "%a, %d %b %Y").day
+                now = datetime.datetime.now().day
+                time_left = abs(date - now)
+                return time_left
+        
+def get_Name(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    episode_name = soup.find_all("h1", {"data-mountable-type": "header"})
+    for episode in episode_name:
+        anime_name = episode.text
+    return anime_name
 
-nuevo_episodio = arreglo_str[contador]
-print(nuevo_episodio)
-cadena = 'data-available="true"'
-if cadena in nuevo_episodio:
+def get_latest_episode(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    episode_elements = soup.find_all("a", {"data-mountable-type": "episode"})
+    latest_episode = "true"
+    episode_count = 0
+    for episode in episode_elements:
+        available = episode.get("data-available")
+        episode_count += 1
+        if available == "true":
+            latest_episode = episode
+        else:
+            break  
+    return latest_episode, episode_count
+
+def send_notification(title, message):
+    # icon_file = r"icon.ico"
     notification = Notify()
-    notification.title = "Anime"
-    notification.message = "El anime esta actualmente."
+    notification.title = title
+    notification.message = message
+    # notification.icon = icon_file
     notification.send()
-else:
-    print("No ha salido")
+
+def main_loop(tiempo_espera):
+    while True:
+        main()
+        time.sleep(tiempo_espera)
+        
+if __name__ == "__main__":
+    tiempo_espera = 15 
+    main_loop(tiempo_espera)
